00a2dc8983efa03984ea3ed92c00dd7308b3c9a9
SpollaL <luca.spolladore@outlook.it>
2026-01-18 20:51:44 +0100

    feat: Add JWT token generation and validation for user authentication

---
+++ internal/auth/jwt.go
@@ -0,0 +1,40 @@
+package auth
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	method := jwt.SigningMethodHS256
+	claims := jwt.RegisteredClaims{
+		Issuer:    "chirpy",
+		IssuedAt:  jwt.NewNumericDate(time.Now().UTC()),
+		ExpiresAt: jwt.NewNumericDate(time.Now().UTC().Add(expiresIn)),
+		Subject:   userID.String(),
+	}
+
+	token := jwt.NewWithClaims(method, claims)
+	stringifiedToken, err := token.SignedString([]byte(tokenSecret))
+	if err != nil {
+		return "", err
+	}
+	return stringifiedToken, nil
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	claims := jwt.RegisteredClaims{}
+	token, err := jwt.ParseWithClaims(tokenString, &claims, func(t *jwt.Token) (any, error) {
+		return []byte(tokenSecret), nil
+	})
+	if err != nil || !token.Valid {
+		return uuid.UUID{}, err
+	}
+	userId, err := uuid.Parse(claims.Subject)
+	if err != nil {
+		return uuid.UUID{}, err
+	}
+	return userId, nil
+}
---
+++ internal/auth/jwt_test.go
@@ -0,0 +1,56 @@
+package auth
+
+import (
+	"testing"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+func TestJwtTokenValidationValid(t *testing.T) {
+	userId, _ := uuid.NewRandom()
+	tokenSecret := "thehorseisonthestable"
+	expiresIn, _ := time.ParseDuration("72h")
+	tokenString, err := MakeJWT(userId, tokenSecret, expiresIn)
+	if err != nil {
+		t.Fatalf("Token generation failed: %v", err)
+	}
+	tokenUserId, err := ValidateJWT(tokenString, tokenSecret)
+	if err != nil {
+		t.Fatalf("Token validation failed: %v", err)
+	}
+	if tokenUserId != userId {
+		t.Fatal("UserID do not match")
+	}
+}
+
+func TestJwtTokenValidationExpired(t *testing.T) {
+	userId, _ := uuid.NewRandom()
+	tokenSecret := "thehorseisonthestable"
+	expiresIn, _ := time.ParseDuration("1ns")
+	tokenString, err := MakeJWT(userId, tokenSecret, expiresIn)
+	if err != nil {
+		t.Fatalf("Token generation failed: %v", err)
+	}
+	time.Sleep(time.Millisecond)
+	tokenUserId, err := ValidateJWT(tokenString, tokenSecret)
+	if err == nil {
+		t.Fatalf("expected error for expired token, got nil (userID=%v)", tokenUserId)
+	}
+}
+
+func TestJwtTokenValidationWrongSecret(t *testing.T) {
+	userId, _ := uuid.NewRandom()
+	tokenSecret := "thehorseisonthestable"
+	tokenSecretWrong := "!thehorseisonthestable"
+	expiresIn, _ := time.ParseDuration("72h")
+	tokenString, err := MakeJWT(userId, tokenSecret, expiresIn)
+	if err != nil {
+		t.Fatalf("Token generation failed: %v", err)
+	}
+	time.Sleep(time.Millisecond)
+	tokenUserId, err := ValidateJWT(tokenString, tokenSecretWrong)
+	if err == nil {
+		t.Fatalf("expected error for wrong secret, got nil (userID=%v)", tokenUserId)
+	}
+}
